9872cb0e71d6b6c636a8e004dd836bc64493e7ec
rica213 <25f5k252l@mozmail.com>
2023-02-14 17:27:28 +0300

    Retrieve shows and display on window after page is loaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import generateId from './modules/generateId.js';
+import retrieveShow from './modules/retrieveShow.js';
+import createDisplay from './modules/createDisplay.js';
+import { shows } from './modules/htmlElements.js';
+import url from './modules/url.js';
+
+
+window.addEventListener('load', (e) => {
+  let id;
+  for(let i = 0; i < 6; i++) {
+    id = generateId(249);
+    retrieveShow(url,id).then((obj) => createDisplay(shows, obj));
+  }
+});
