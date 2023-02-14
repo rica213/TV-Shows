a43125fa7fc703e644516e1b9814dbac73f09a1a
rica213 <25f5k252l@mozmail.com>
2023-02-14 15:11:57 +0300

    Add function to create display

---
+++ src/modules/createDisplay.js
@@ -0,0 +1,8 @@
+const createDisplay = (container, imgUrl, name) => {
+  const li = document.createElement('li');
+  li.innerHTML = `<img src="${imgUrl}" alt="${name}"/>
+  <p class="name">${name}</p>
+  <button type="button">Comment</button>
+  <button type="reservation">Reservation</button>`;
+  container.appendChild(li);
+}
