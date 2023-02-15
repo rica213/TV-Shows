10539b123e84bc7fb0110df1e8452ad40f9f180f
rica213 <25f5k252l@mozmail.com>
2023-02-15 19:45:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import generateId from './modules/generateId.js';
 import retrieve from './modules/retrieve.js';
 import createDisplay from './modules/createDisplay.js';
 import { shows, modal, overlay } from './modules/htmlElements.js';
-import { urlShow, urlInvolvement } from './modules/url.js';
+import { urlShow } from './modules/url.js';
 import openModal from './modules/displayModal.js';
 import closeModal from './modules/closeModal.js';
 import init from './modules/init.js';
@@ -12,9 +12,9 @@ const ids = [];
 window.addEventListener('load', () => {
   let id;
   for (let i = 0; i < 6; i += 1) {
-    id = generateId(249);
+    id = generateId(10);
     ids.push(id);
-    retrieve(`${urlShow}shows/${id}`, id).then((obj) => createDisplay(shows, obj));
+    retrieve(`${urlShow}shows/${id}`).then((obj) => createDisplay(shows, obj));
   }
 });
 
@@ -23,7 +23,7 @@ shows.addEventListener('click', (e) => {
     init(modal);
     ids.forEach((id) => {
       if (Number(e.target.id) === id) {
-        retrieve(`${urlShow}shows/${id}`, id).then((data) => openModal(modal, data, overlay));
+        retrieve(`${urlShow}shows/${id}`).then((data) => openModal(modal, data, overlay));
       }
     });
   }
