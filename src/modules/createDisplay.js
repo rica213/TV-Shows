a28cef34fa64441d07800461efa99c96902fe052
rica213 <25f5k252l@mozmail.com>
2023-02-15 18:45:15 +0300

    Remove redundant code

--- src/modules/createDisplay.js
+++ src/modules/createDisplay.js
@@ -1,14 +1,13 @@
 const createDisplay = (container, obj) => {
   const li = document.createElement('li');
-  li.innerHTML = `<div class = "show-modal" >
+  li.classList.add('show-modal');
+  li.innerHTML = `
                     <img src="${obj.image.medium}" alt="${obj.name}"/>
-                  </div>
                   <p class="name">${obj.name}</p>
                   <i class="fa-regular fa-heart like"></i>
                   <span class="nb-likes"></span>
                   <div class="box">
-                    <i class="fa-regular fa-heart like"></i><br/>
-  <button type="button" class="comment-btn" id="${obj.id}">Comment</button>
+                    <button type="button" class="comment-btn" id="${obj.id}">Comment</button>
                     <button type="button" class="comment-rev">Reservation</button>
                   </div>`;
   container.appendChild(li);
