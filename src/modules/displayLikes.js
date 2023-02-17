81d553425338f0416155081acdbffb10a6d7c491
rica213 <25f5k252l@mozmail.com>
2023-02-17 12:02:03 +0300

    Define a function that display likes

---
+++ src/modules/displayLikes.js
@@ -0,0 +1,8 @@
+const displayLikes = ({element, id, nbOfLikes}) => {
+  const child = Array.from(element.children).find((item) => Number(item.id) === id);
+  if (child) {
+    child.querySelector('span.nb-likes').innerHTML = `${nbOfLikes} Likes`;
+  }
+};
+
+export default displayLikes;
